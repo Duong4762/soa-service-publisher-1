f9bc54aa14f6dc37e3f38f7b057e54117771c249
Duongnt <duongtungnguyen88@gmail.com>
2026-03-10 10:44:27 +0700

    init

---
+++ src/main/java/org/example/servicepublisher1/ServicePublisher1Application.java
@@ -0,0 +1,13 @@
+package org.example.servicepublisher1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePublisher1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePublisher1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/servicepublisher1/ServicePublisher1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicepublisher1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePublisher1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
